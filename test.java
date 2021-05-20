9e3de83c165fb4296535135b23d8e56536a07619
fangchangshu <84385766+fangchangshu@users.noreply.github.com>
2021-05-20 14:44:04 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,13 @@
+package testone;
+
+public class one1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a,b,c;
+        a=1,b=2;
+        c=a+b;
+    System.out.println("c="+c);
+	}
+
+}
